2d2ea7014bd92c5a5ee4c3f215d984018c2c092a
Pradeep4u2 <128627674+Pradeep4u2@users.noreply.github.com>
2025-11-12 14:43:00 -0500

    alb updated

--- main.tf
+++ main.tf
@@ -45,6 +45,17 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.2"
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  # insert the 1 required variable here
+}
+
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
@@ -53,35 +64,20 @@ module "blog_alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
   target_groups = {
-    ex-instance = {
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
       target_id        = "aws_instance.blog.id"
     }
+
+    http_tcp_listeners = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+
   }
 
   tags = {
